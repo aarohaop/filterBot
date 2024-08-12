424cbafa1eebea5294e8d67e7b08f3364dbdcfde
aarohaop <152183446+aarohaop@users.noreply.github.com>
2024-08-12 18:14:58 +0545

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -2867,9 +2867,9 @@ async def auto_filter(client, name, msg, reply_msg, ai_search, spoll=False):
                 cap += f"<b>\n📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n</a></b>"
     else:
         if settings["button"]:
-            cap = f"<b><b>HEY {query.from_user.mention}🍿\n\n📁The Results Fᴏʀ Your Search ☞ {search}\n\n</b>"
+            cap = f"<b>HEY {query.from_user.mention}🍿\n\n📁The Results Fᴏʀ Your Search ☞ {search}\n\n</b>"
         else:
-            cap = f"<b><b>HEY {query.from_user.mention}🍿\n\n📁The Results Fᴏʀ Your Search ☞ {search}\n\n</b>"
+            cap = f"<b>HEY {query.from_user.mention}🍿\n\n📁The Results Fᴏʀ Your Search ☞ {search}\n\n</b>"
             cap+="<b><u>🍿 Your Movie Files 👇</u></b>\n\n"
             for file in files:
                 cap += f"<b>📁 <a href='https://telegram.me/{temp.U_NAME}?start=files_{file.file_id}'>[{get_size(file.file_size)}] {' '.join(filter(lambda x: not x.startswith('[') and not x.startswith('@') and not x.startswith('www.'), file.file_name.split()))}\n\n</a></b>"
