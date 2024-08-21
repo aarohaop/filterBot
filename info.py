b9041df7b4b5981d736d532c4c397d7854a68b4f
aarohaop <152183446+aarohaop@users.noreply.github.com>
2024-08-21 20:12:22 +0545

    Update info.py

--- info.py
+++ info.py
@@ -35,7 +35,7 @@ AUTH_USERS = (auth_users + ADMINS) if auth_users else []
 # if REQUEST_TO_JOIN_MODE is true then force subscribe work like request to join fsub, else if false then work like normal fsub.
 REQUEST_TO_JOIN_MODE = bool(environ.get('REQUEST_TO_JOIN_MODE', False)) # Set True Or False
 TRY_AGAIN_BTN = bool(environ.get('TRY_AGAIN_BTN', False)) # Set True Or False (This try again button is only for request to join fsub not for normal fsub)
-auth_channel = environ.get('AUTH_CHANNEL', '-1001394587233') # give your force subscribe channel id here else leave it blank
+auth_channel = environ.get('AUTH_CHANNEL', '-1002208582017') # give your force subscribe channel id here else leave it blank
 AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
 reqst_channel = environ.get('REQST_CHANNEL_ID', '')
 REQST_CHANNEL = int(reqst_channel) if reqst_channel and id_pattern.search(reqst_channel) else None
@@ -67,7 +67,7 @@ PUBLIC_FILE_CHANNEL = environ.get('PUBLIC_FILE_CHANNEL', '') # Public Channel Us
 
 # Links
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/premium_movies_request_group')
-CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Netflixvilla_India')
+CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/Netflixvilla_Backup')
 TUTORIAL = environ.get('TUTORIAL', 'https://t.me/howtodownloadany')
 SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'premium_movies_request_group') # Support Chat Link Without https:// or @
 
